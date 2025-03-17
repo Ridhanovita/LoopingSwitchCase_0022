1044959655c00c56ae880c7ec5c630051d4a2e3a
Ridhanovita <novitaridha796@gmail.com>
2025-03-17 12:03:14 +0700

    Menambahkan variabel serta perulangan do while

--- contohSwitchCase.cpp
+++ contohSwitchCase.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 using namespace std;
 
-float luasPersegiPanjang(float a, float b){
+float luasPersegiPanjang(float a, float b)
+{
     return a * b;
 }
 
-float luasLingkaran(float a){
-    return 3.14*a*a;
+float luasLingkaran(float a)
+{
+    return 3.14 * a * a;
+}
+
+int main()
+{
+    int pilihan;
+    float panjang, lebar;
+    float jejari;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihlah antara 1/2/3 = ";
+        cin >> pilihan;
+
+       
+    } while (pilihan != 3);
 }
